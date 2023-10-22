5964639281b847468dcb20910eaa46c5af618cbd
divyada.patil <divyada.patil@outlook.com>
2023-10-22 11:15:59 -0400

    Baseline

--- AzWebApp/Pages/Index.cshtml.cs
+++ AzWebApp/Pages/Index.cshtml.cs
@@ -6,16 +6,11 @@ namespace AzWebApp.Pages
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
-		private readonly IConfiguration _configuration;
 
-		public int IndexKey => _configuration.GetValue<int>("IndexKey");
-
-		public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+		public IndexModel(ILogger<IndexModel> logger)
 		{
 			_logger = logger;
-            _configuration = configuration;
-
-        }
+		}
 
 		public void OnGet()
 		{
