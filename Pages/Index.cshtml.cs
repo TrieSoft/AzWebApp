5cd29d1d6f5a036c1077a7d875d9fcec907b3c03
divyada.patil <divyada.patil@outlook.com>
2023-10-22 11:05:41 -0400

    Baseline

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzWebApp.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		private readonly IConfiguration _configuration;
+
+		public int IndexKey => _configuration.GetValue<int>("IndexKey");
+
+		public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+            _configuration = configuration;
+
+        }
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
